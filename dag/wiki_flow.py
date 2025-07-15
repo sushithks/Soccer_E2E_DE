cdd1be84bd4f463055880d5e4a08f3484e5df479
sushith <sushithks@gmail.com>
2025-07-15 19:14:30 -0400

    Orchestration- second task.1

--- dag/wiki_flow.py
+++ dag/wiki_flow.py
@@ -6,7 +6,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from main.data_fetch import extract_wikipedia_data
+from main.data_fetch import extract_wikipedia_data, transform_wikipedia_data
 
 dag = DAG(
     dag_id='wikipedia_flow',
@@ -24,5 +24,12 @@ extract_data_from_wikipedia = PythonOperator(
     python_callable=extract_wikipedia_data,
     provide_context=True,
     op_kwargs={"url": "https://en.wikipedia.org/wiki/List_of_association_football_stadiums_by_capacity"},
+    dag=dag
+)
+
+transform_wikipedia_data = PythonOperator(
+    task_id='transform_wikipedia_data',
+    provide_context=True,
+    python_callable=transform_wikipedia_data,
     dag=dag
 )
