6f537b507bc4e1c807b07dcc61b13e5a02806852
sushith <sushithks@gmail.com>
2025-07-27 21:34:55 -0400

    stadium count test case

--- Test/test_case.py
+++ Test/test_case.py
@@ -29,5 +29,31 @@ class TestTopStadium(unittest.TestCase):
         # Reset index for comparison (optional)
         pd.testing.assert_frame_equal(result.reset_index(drop=True), expected.reset_index(drop=True))
 
+
+
+class stadium_count(unittest.TestCase):
+    def test_returns_stadium_count(self):
+        # Sample input data
+        data = [
+            {'rank': 1, 'stadium': 'Michigan Stadium', 'capacity': 90000},
+            {'rank': 2, 'stadium': 'Wembley Stadium', 'capacity': 80000},
+            {'rank': 3, 'stadium': 'stadio Azteca', 'capacity': 75000},
+            {'rank': 4, 'stadium': 'Croke Park', 'capacity': 70000},
+            {'rank': 5, 'stadium': 'Shah Alam Stadium', 'capacity': 85000},
+            {'rank': 6, 'stadium': 'Empower Field', 'capacity': 60000},
+        ]
+
+        # Expected output DataFrame
+        expected = pd.DataFrame([
+
+        ])
+
+        # Call the function
+        result = stadium_count(data)
+
+        # Reset index for comparison (optional)
+        pd.testing.assert_frame_equal(result.reset_index(drop=True), expected.reset_index(drop=True))
+
+
 if __name__ == '__main__':
     unittest.main()
